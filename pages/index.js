51da25a0057b2e5f3f82073bfc474a4617d1761d
pi3rrot <4dakto@gmail.com>
2018-10-06 21:55:38 +0900

    Link를 이용한 라우팅.

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-export default () => <div>Welcome to next.js!</div>
+import Link  from 'next/link'
+
+export default () => (
+  <div>
+    <Link href="/page2">
+      <button>go to page2</button>
+    </Link>
+  </div>
+);
