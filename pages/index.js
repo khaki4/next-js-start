7ec33a8366c200eddb2fa5c2617e7d0dbff523fe
pi3rrot <4dakto@gmail.com>
2018-10-06 22:23:09 +0900

    동적 페이지 다루기

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import Link from 'next/link'
 import Header from '../components/Header'
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 export default () => (
   <div>
     <Header />
     <p>Hello Next.js</p>
+    <ul>
+      <PostLink title={"hello"} />
+      <PostLink title={"hello2"} />
+      <PostLink title={"hello3"} />
+    </ul>
   </div>
 );
