2dc104581e5a2ac00159a0a0f7bb262300ac9564
pi3rrot <4dakto@gmail.com>
2018-10-06 23:34:06 +0900

    데이터 가져오기

--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,30 @@
-export default (props) => (
-  <div>
-    <h1>{props.url.query.title}</h1>
-    <p>This is the blog post content.</p>
-  </div>
-)
+import fetch from 'isomorphic-unfetch';
+
+const Post = (props) => {
+  console.log('post render')
+  return (
+    <div>
+      <h1>{props.show.name}</h1>
+      <img src={props.show.image.medium} alt=""/>
+      <div dangerouslySetInnerHTML={{ __html: props.show.summary }} />
+    </div>
+  )
+}
+
+const fakeApiForPrefetchTest = async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => { resolve('done') }, 2000)
+  })
+}
+
+Post.getInitialProps = async (context) => {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default Post
